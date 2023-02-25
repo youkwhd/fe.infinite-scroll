e315357c347a6947bfc86b0b9d9307caf86d29dc
youkwhd <lolywk@tutanota.com>
2023-02-25 16:25:53 +0700

    chore: initial commit

---
+++ src/App.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+</script>
+
+<main>
+    fe.infinite-scroll
+</main>
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import App from "./App.svelte"
+
+const app = new App({ target: document.getElementById("__root") })
+
+export default app
